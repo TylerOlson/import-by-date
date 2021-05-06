b7438990ae35d5b52c5fbd3ccd2e3896d507c10a
Tyler Olson <tolson519@gmail.com>
2021-05-05 17:14:13 -0700

    FTP upload and bug fixes

--- main.py
+++ main.py
@@ -7,41 +7,47 @@ import pathlib
 import tkinter as tk
 from tkinter import filedialog
 import ftplib
+import time
 
 
 def main():
-    """
     parser = argparse.ArgumentParser(
         description='Move files in order of date taken.')
-    parser.add_argument('originfolder', type=str, help='folder to move from')
-    parser.add_argument('destination_folder', type=str,
-                        help='folder to move to')
+    parser.add_argument('-ftp',
+                        help='use ftp transfer', action='store_true')
+    parser.add_argument(
+        '--ip', type=str, help='ip to use for ftp transfer', default='192.168.1.107')
+    parser.add_argument('--port', type=int,
+                        help='port to use for ftp transfer', default=2221)
 
     args = parser.parse_args()
-
-    args.originfolder = pathlib.Path(args.originfolder)
-    args.destination_folder = os.path.abspath(args.destination_folder)
-    """
-
-    server = ftplib.FTP()
-    server.connect('192.168.1.107', 2221)
-    server.login('android', 'android')
-    server.dir()
+    server = None
+    destination_folder = None
 
     print("Select source folder")
-    #originfolder = draw_file_dialog()
-    originfolder = 'C:/Users/tolso/Documents/Coding/python/import_by_date/pictures'
+    originfolder = draw_file_dialog()
+    #originfolder = 'C:/Users/tolso/Documents/Coding/python/import-by-date/pictures'
     print(originfolder)
 
-    print("Select destination folder")
-    #destination_folder = draw_file_dialog()
-    destination_folder = 'C:/Users/tolso/Documents/Coding/python/import_by_date/destination'
-    print(destination_folder)
+    if args.ftp:
+        server = ftplib.FTP()
+        server.connect(args.ip, args.port)
+        server.login('android', 'android')
+        server.dir()
+    else:
+        print("Select destination folder")
+        destination_folder = draw_file_dialog()
+        # destination_folder = 'C:/Users/tolso/Documents/Coding/python/import-by-date/destination'
+        print(destination_folder)
 
     all_files = get_all_files(originfolder)
     for file in all_files:
         print(file)
-    copy_files(all_files, destination_folder)
+
+    copy_files(all_files, destination_folder, server, args.ftp)
+
+    if args.ftp:
+        server.close()
 
 
 def get_all_files(picture_path):
@@ -57,22 +63,35 @@ def get_all_files(picture_path):
     return all_files
 
 
-def copy_files(all_files, destination_folder):
-    if os.path.isfile(destination_folder):
-        print("Desination folder is a file")
-        return
-    if not os.path.isdir(destination_folder):
-        os.makedirs(os.path.dirname(destination_folder), exist_ok=True)
-    for file_to_move in all_files:
-        full_destination_folder = os.path.join(
-            destination_folder, file_to_move['filename'])
-        os.makedirs(os.path.dirname(destination_folder), exist_ok=True)
+def copy_files(all_files, destination_folder, server, ftp):
+    if not ftp:
+        if os.path.isfile(destination_folder):
+            print("Desination folder is a file")
+            return
+        if not os.path.isdir(destination_folder):
+            print("Not found")
+            return
+        for file_to_move in all_files:
+            full_destination_folder = os.path.join(
+                destination_folder, file_to_move['filename'])
+            os.makedirs(os.path.dirname(destination_folder), exist_ok=True)
 
-        print("Copying " + file_to_move['full_path'] +
-              " to " + full_destination_folder)
-        shutil.copyfile(file_to_move['full_path'], full_destination_folder)
-        print("Copied " + file_to_move['full_path'] +
-              " to " + full_destination_folder)
+            print("Copying " + file_to_move['full_path'] +
+                  " to " + full_destination_folder)
+            shutil.copyfile(file_to_move['full_path'], full_destination_folder)
+            shutil.copystat(file_to_move['full_path'], full_destination_folder)
+            print("Copied " +
+                  file_to_move['full_path'] + " to " + full_destination_folder)
+    else:
+        for file_to_move in all_files:
+            final_path = os.path.join(
+                'DCIM', 'Restored', file_to_move['filename'])
+            print("Copying " + final_path + " to FTP server")
+            binary = open(file_to_move['full_path'], 'rb')
+            print(server.storbinary("STOR " + final_path, binary))
+            binary.close()
+            print("Copied " + final_path + " to FTP server")
+            time.sleep(1)
 
 
 def draw_file_dialog():
@@ -89,5 +108,3 @@ def sort_func(e):
 
 if __name__ == "__main__":
     main()
-
-#            print(full_path) date = datetime.fromtimestamp(os.path.getmtime(full_path))
